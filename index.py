36df17f9f97ddba1922cb2a576df3eda64c00f02
Fournierp <paul.fournier4@mail.mcgill.ca>
2019-01-09 12:15:40 -0500

    app layout

---
+++ index.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+import flask
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+from app import app, server
+from apps import research
+
+
+app.layout = html.Div([
+    html.H1('Alfred Virtual Assistant'),
+
+    html.Div('''
+        Alfred: A web application to assist you with your stock investments.
+    '''),
+
+        html.Div([
+
+            dcc.Tabs(
+                id="tabs",
+                style={"height":"20","verticalAlign":"middle"},
+                children=[
+                    dcc.Tab(label="Live", value="one"),
+                    dcc.Tab(label="Research", value="two"),
+                    dcc.Tab(label="Predictions", value="three"),
+                ],
+                value="one",
+            )
+
+            ],
+            className="row tabs_div"
+        ),
+
+        html.Div(id="tab_content", className="row", style={"margin": "2% 3%"}),
+
+])
+
+
+@app.callback(
+    Output("tab_content", "children"),
+    [Input("tabs", "value")])
+def render_content(tab):
+    if tab == "one":
+        return research.layout
+    elif tab == "two":
+        return html.Div([
+            html.H3('Tab content 2')
+        ])
+    elif tab == "three":
+        return html.Div([
+            html.H3('Tab content 3')
+        ])
+    else:
+        return research.layout
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
