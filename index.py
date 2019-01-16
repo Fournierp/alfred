4969013c2d1aa5cd74bd8c5610c5dd6cb4fc2dae
unknown <fournier.phk@gmail.com>
2019-01-15 20:41:08 -0500

    ui

--- index.py
+++ index.py
@@ -16,24 +16,26 @@ app.layout = html.Div([
         Alfred: A web application to assist you with your stock investments.
     '''),
 
-        html.Div([
+    html.Br(),
 
-            dcc.Tabs(
-                id="tabs",
-                style={"height":"20","verticalAlign":"middle"},
-                children=[
-                    dcc.Tab(label="Live", value="one"),
-                    dcc.Tab(label="Research", value="two"),
-                    dcc.Tab(label="Predictions", value="three"),
-                ],
-                value="one",
-            )
+    html.Div([
 
+        dcc.Tabs(
+            id="tabs",
+            style={"height":"20","verticalAlign":"middle"},
+            children=[
+                dcc.Tab(label="Live", value="one"),
+                dcc.Tab(label="Research", value="two"),
+                dcc.Tab(label="Predictions", value="three"),
             ],
-            className="row tabs_div"
-        ),
+            value="two",
+        )
 
-        html.Div(id="tab_content", className="row", style={"margin": "2% 3%"}),
+        ],
+        className="row tabs_div"
+    ),
+
+    html.Div(id="tab_content", className="row", style={"margin": "2% 3%"}),
 
 ])
 
@@ -43,11 +45,11 @@ app.layout = html.Div([
     [Input("tabs", "value")])
 def render_content(tab):
     if tab == "one":
-        return research.layout
-    elif tab == "two":
         return html.Div([
-            html.H3('Tab content 2')
+            html.H3('Tab content 1')
         ])
+    elif tab == "two":
+        return research.layout
     elif tab == "three":
         return html.Div([
             html.H3('Tab content 3')
