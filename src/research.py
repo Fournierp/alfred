b9449c31f3b6e01c3061791e6179300911ddfd9c
Paul Fournier <fournier.phk@gmail.com>
2020-11-09 15:15:18 +0100

    company scraping/display

--- src/research.py
+++ src/research.py
@@ -1,4 +1,19 @@
 import streamlit as st
+import pandas as pd
+import yfinance as yf
+from datetime import datetime
+
+
+@st.cache
+def load_data():
+    companies = pd.read_html('https://en.wikipedia.org/wiki/List_of_S'
+                             '%26P_500_companies')[0]
+    return companies.drop('SEC filings', axis=1).set_index('Symbol')
+
+
+@st.cache()
+def load_quotes(asset):
+    return yf.download(asset)
 
 
 def write():
@@ -10,3 +25,23 @@ def write():
             """,
             unsafe_allow_html=True,
         )
+
+        companies = load_data()
+
+        # Show table of companies
+        if st.checkbox('View companies list', value=True):
+            option = st.selectbox(
+                'Which sectors should be displayed',
+                ('All',) + tuple(companies['GICS Sector'].unique()),
+                index=0)
+
+            if option != 'All':
+                st.dataframe(companies[['Security',
+                                        'GICS Sector',
+                                        'Date first added',
+                                        'Founded']][companies['GICS Sector'] == option])
+            else:
+                st.dataframe(companies[['Security',
+                                        'GICS Sector',
+                                        'Date first added',
+                                        'Founded']])
